01220cdd9ebb2bc38624b70c3bd9ed1ac5f3eb18
Corey McCormick <corey@simplesoftware.io>
2016-05-30 15:12:12 -0400

    Create DoesNotReceieve trait for drivers who do not support incoming messages

---
+++ src/SimpleSoftwareIO/SMS/DoesNotReceive.php
@@ -0,0 +1,42 @@
+<?php
+namespace SimpleSoftwareIO\SMS;
+
+trait DoesNotReceive
+{
+    /**
+     * Checks the server for messages and returns their results.
+     *
+     * @param array $options
+     *
+     * @return array
+     */
+    public function checkMessages(array $options = [])
+    {
+        throw new \RuntimeException('Receive methods are not support with the E-Mail driver.');
+    }
+
+    /**
+     * Gets a single message by it's ID.
+     *
+     * @param string|int $messageId
+     *
+     * @throws \RuntimeException
+     *
+     */
+    public function getMessage($messageId)
+    {
+        throw new \RuntimeException('Receive methods are not support with the E-Mail driver.');
+    }
+
+    /**
+     * Receives an incoming message via REST call.
+     *
+     * @param mixed $raw
+     *
+     * @throws \RuntimeException
+     */
+    public function receive($raw)
+    {
+        throw new \RuntimeException('Receive methods are not support with the E-Mail driver.');
+    }
+}
